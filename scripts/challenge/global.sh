135619f8dae0418b3417f6abbf825aaa29608139
morton311 <cmorton9053@gmail.com>
2026-07-15 11:04:28 -0400

    update

--- scripts/challenge/global.sh
+++ scripts/challenge/global.sh
@@ -2,17 +2,17 @@
 # JOB HEADERS HERE
 #SBATCH --job-name=challenge_global
 #SBATCH --account=NAWCP24632466
-#SBATCH --qos=standard
+#SBATCH --qos=debug
 #SBATCH --constraint=mla
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH -t 24:00:00
+#SBATCH -t 00:30:00
 #SBATCH --output=out/challenge_global.out
 
 module use $HOME/my_modules
 module load torch_module
 source $HOME/.venv/bin/activate
 
-torchrun main.py -c challenge/global2 -m train -d True
+# torchrun main.py -c challenge/global2 -m train -d True
 python main.py -c challenge/global2 -m pred
 python main.py -c challenge/global2 -m eval
